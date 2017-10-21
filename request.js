99b37c657ec7f14851d352cf9ad474e3e890832c
Nathan Mukena <Nathan Mukena>
2017-10-21 17:15:33 -0400

    Performed extractions of all issues

--- request.js
+++ request.js
@@ -6,8 +6,8 @@ request = require('request');
 var options = {rejectUnauthorized: this.strictSSL, 
     uri: "https://mehran-development.atlassian.net/rest/api/2/issue/GTMP-2", 
     method: 'GET',
-    auth: {'user': 'USERNAME', 
-    'pass': 'PASSWORD'}
+    auth: {'user': 'nmukena@deloitte.ca', 
+    'pass': 'I lift my eyes up.'}
 };
 
 getIssue = function(issueNumber) {
@@ -32,6 +32,32 @@ getIssue = function(issueNumber) {
                 console.log(JSON.parse(body));
         });
 };
+//Attempt to use express.
+app.get("/getAllIssues/:projectId", function(req, res)  {
+    options.uri = "https://mehran-development.atlassian.net/rest/api/2/search?jql=project="+req.params.projectId; 
+    request(options, function(error, response, body) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        if (response.statusCode === 404) {
+            console.log('Invalid issue number.');
+            return;
+        }
+        if (response.statusCode !== 200) {
+            console.log(response.statusCode + ': Unable to connect to JIRA during findIssueStatus.');
+            return;
+        }
+        if (body === undefined) {
+            console.log('Response body was undefined.');
+            return;
+        }
+        json = JSON.parse(body);
+        console.log(json);
+        res.json(json);
+    });
+});
+
 
 getAllIssuesProject = function(projectId) {
     options.uri = "https://mehran-development.atlassian.net/rest/api/2/search?jql=project="+projectId;
@@ -55,6 +81,8 @@ getAllIssuesProject = function(projectId) {
             console.log(JSON.parse(body));
     });
 };
+
+
 
 
 getIssueMeta = function(issueNumber) {
@@ -148,3 +176,7 @@ getProjectRoles = function(projectId) {
             console.log(JSON.parse(body));
     });
 };
+
+app.listen(3000, function() {  
+    console.log("Request Server is running on http://localhost:3000");
+});
