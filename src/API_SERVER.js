46b40316febfb59540def04fa5c05083343e1c1c
Samuel <samuelwlai@gmail.com>
2018-04-29 09:58:33 -0400

    aws

--- src/API_SERVER.js
+++ src/API_SERVER.js
@@ -1,7 +1,3 @@
 export default function api_server(){
-<<<<<<< HEAD:src/js/API_SERVER.js
     return 'http://18.221.174.71:3000';
-=======
-    return 'http://localhost:5000';
->>>>>>> origin/mvp5:src/API_SERVER.js
 }
