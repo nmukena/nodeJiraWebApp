4a4349412d27faa7536f8854dfabd3c84babaa86
Samuel <samuelwlai@gmail.com>
2018-05-01 16:45:22 -0400

    server

--- src/API_SERVER.js
+++ src/API_SERVER.js
@@ -1,8 +1,3 @@
 export default function api_server(){
-<<<<<<< HEAD
     return 'http://18.221.174.71:3000';
-=======
-    // return 'http://18.221.174.71:3000';
-    return 'http://localhost:5000';
->>>>>>> origin/mvp5
 }
