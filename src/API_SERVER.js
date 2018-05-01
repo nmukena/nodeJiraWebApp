f6534bcb8b1ba4cccc5528562d0ffe29007656c0
Ubuntu <ubuntu@ip-172-31-28-81.us-east-2.compute.internal>
2018-05-01 21:00:13 +0000

    server

--- src/API_SERVER.js
+++ src/API_SERVER.js
@@ -1,4 +1,4 @@
 export default function api_server(){
-    // return 'http://18.221.174.71:3000';
-    return 'http://localhost:5000';
+    return 'http://18.221.174.71:3000';
+    // return 'http://localhost:5000';
 }
